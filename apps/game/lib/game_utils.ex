6de0797521936e162320a36a63c8a03f1f62fc0f
Mario Mainz <mario.mainz@invision.de>
2016-09-25 21:23:02 +0200

    Add GameUtils function to generate random states

--- apps/game/lib/game_utils.ex
+++ apps/game/lib/game_utils.ex
@@ -3,6 +3,19 @@ defmodule GameUtils do
     "\n" <> state_string(state) <> "\n"
   end
 
+  def random_state(size), do: random_state(size, size)
+  def random_state(width, height) do
+    fn -> generate_row(width) end
+    |> Stream.repeatedly
+    |> Enum.take(height)
+  end
+
+  defp generate_row(width) do
+    fn -> Enum.random([true, false]) end
+    |> Stream.repeatedly
+    |> Enum.take(width)
+  end
+
   defp state_string(state) do
     state
     |> Enum.map(fn row -> "|" <> row_string(row) <> "|" end)
--- apps/game/test/game_utils_test.exs
+++ apps/game/test/game_utils_test.exs
@@ -10,4 +10,18 @@ defmodule GameUtilsTest do
 
     assert expected_result == GameUtils.to_printable(@state)
   end
+
+  test "creates random states with specified size" do
+    state = GameUtils.random_state(3)
+
+    assert 3 == length(state)
+    assert Enum.all?(state, fn row -> length(row) == 3 end)
+  end
+
+  test "creates random states with specified width and height" do
+    state = GameUtils.random_state(2, 4)
+
+    assert 4 == length(state)
+    assert Enum.all?(state, fn row -> length(row) == 2 end)
+  end
 end
