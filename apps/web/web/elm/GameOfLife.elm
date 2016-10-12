07425fa15d70ef4e312f8373fa47f12a2e73cf7f
Mario Mainz <mario.mainz@invision.de>
2016-10-12 23:52:16 +0200

    Consume game updates on client

--- apps/web/web/elm/GameOfLife.elm
+++ apps/web/web/elm/GameOfLife.elm
@@ -4,23 +4,34 @@ import Html exposing (..)
 import Html.App
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
+import Json.Decode exposing ((:=))
 import Json.Encode
-import Phoenix.Socket
 import Phoenix.Channel
 import Phoenix.Push
+import Phoenix.Socket
 import String
 
 type alias Model =
     { phxSocket : Phoenix.Socket.Socket Msg
     , width : Int
     , height : Int
+    , name : String
     }
+
+type alias NewGameNotification = { name : String }
+
+newGameNotificationDecoder : Json.Decode.Decoder NewGameNotification
+newGameNotificationDecoder =
+    Json.Decode.object1 NewGameNotification
+        ("name" := Json.Decode.string)
 
 type Msg =
     SetWidth String
     | SetHeight String
+    | SetName String
     | CreateGame
     | PhoenixMsg (Phoenix.Socket.Msg Msg)
+    | ReceiveNewGame Json.Encode.Value
 
 main : Program Never
 main =
@@ -31,16 +42,21 @@ main =
         , subscriptions = subscriptions
         }
 
+initSocket : Phoenix.Socket.Socket Msg
+initSocket =
+    Phoenix.Socket.init "ws://localhost:4000/socket/websocket"
+        |> Phoenix.Socket.on "new_game" "game:lobby" ReceiveNewGame
+
 init : ( Model, Cmd Msg )
 init =
     let
-        initSocket = Phoenix.Socket.init "ws://localhost:4000/socket/websocket"
         channel = Phoenix.Channel.init "game:lobby"
         (phxSocket, phxCmd) = Phoenix.Socket.join channel initSocket
         initModel =
             { phxSocket = phxSocket
             , width = 0
             , height = 0
+            , name = "MyGame"
             }
     in
         ( initModel, Cmd.map PhoenixMsg phxCmd )
@@ -49,10 +65,11 @@ view : Model -> Html Msg
 view model =
     div []
         [ p [] [text "New Game of Life"]
-        , p [] [input [onInput SetWidth, value (toString model.width)] []
+        , p [] [ input [onInput SetWidth, value (toString model.width)] []
                , text "x"
                , input [onInput SetHeight, value (toString model.height)] []
                ]
+        , p [] [input [onInput SetName, value model.name] []]
         , p [] [button [onClick CreateGame] [text "Create!"]]
         ]
 
@@ -81,12 +98,18 @@ update msg model =
                   , Cmd.none
                   )
 
+        SetName name ->
+            ( { model | name = name }
+            , Cmd.none
+            )
+
         CreateGame ->
             let
               payload =
                   Json.Encode.object
-                        [ ("width", Json.Encode.string (toString model.width))
-                        , ("height", Json.Encode.string (toString model.height))
+                        [ ("width", Json.Encode.int model.width)
+                        , ("height", Json.Encode.int model.height)
+                        , ("name", Json.Encode.string model.name)
                         ]
               push' =
                 Phoenix.Push.init "create" "game:lobby"
@@ -104,6 +127,22 @@ update msg model =
             ( { model | phxSocket = phxSocket }
             , Cmd.map PhoenixMsg phxCmd
             )
+
+        ReceiveNewGame raw ->
+            case Json.Decode.decodeValue newGameNotificationDecoder raw of
+                Ok newGameNotification ->
+                    let
+                      channel = Phoenix.Channel.init ("game:" ++ newGameNotification.name)
+                      (phxSocket, phxCmd) = Phoenix.Socket.join channel model.phxSocket
+                    in
+                      ( { model | phxSocket = phxSocket }
+                      , Cmd.map PhoenixMsg phxCmd
+                      )
+
+                Err _ ->
+                    ( model
+                    , Cmd.none
+                    )
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
