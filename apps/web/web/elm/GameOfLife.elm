3ef5dce09023153a471a82094ec878e563b3f8e4
Mario Mainz <mario.mainz@invision.de>
2016-10-23 22:32:21 +0200

    Render the state of the current game

--- apps/web/web/elm/GameOfLife.elm
+++ apps/web/web/elm/GameOfLife.elm
@@ -7,7 +7,6 @@ import Html.Events exposing (..)
 import Json.Decode exposing ((:=))
 import Json.Encode
 import Phoenix.Channel
-import Phoenix.Push
 import Phoenix.Socket
 import String
 
@@ -17,14 +16,23 @@ type alias Model =
     , height : Int
     , name : String
     , games : List String
+    , gameState : List (List Bool)
+    , currentGame : Maybe String
     }
 
 type alias NewGameNotification = { name : String }
+
+type alias StateUpdate = { state : List (List Bool) }
 
 newGameNotificationDecoder : Json.Decode.Decoder NewGameNotification
 newGameNotificationDecoder =
     Json.Decode.object1 NewGameNotification
         ("name" := Json.Decode.string)
+
+stateUpdatedDecoder : Json.Decode.Decoder StateUpdate
+stateUpdatedDecoder =
+    Json.Decode.object1 StateUpdate
+        ("state" := (Json.Decode.list (Json.Decode.list Json.Decode.bool)))
 
 type Msg =
     SetWidth String
@@ -33,6 +41,7 @@ type Msg =
     | CreateGame
     | PhoenixMsg (Phoenix.Socket.Msg Msg)
     | ReceiveNewGame Json.Encode.Value
+    | StateUpdated Json.Encode.Value
 
 main : Program Never
 main =
@@ -59,6 +68,8 @@ init =
             , height = 10
             , name = "MyGame"
             , games = []
+            , gameState = []
+            , currentGame = Nothing
             }
     in
         ( initModel, Cmd.map PhoenixMsg phxCmd )
@@ -66,22 +77,40 @@ init =
 view : Model -> Html Msg
 view model =
     let
-      gameItems = List.map gameItem model.games
+      gameItems = ul [] (List.map gameItem model.games)
+      stateTable = table [] (List.map stateRow model.gameState)
     in
-      div []
-          [ p [] [text "New Game of Life"]
-          , p [] [ input [onInput SetWidth, value (toString model.width)] []
-                , text "x"
-                , input [onInput SetHeight, value (toString model.height)] []
-                ]
-          , p [] [input [onInput SetName, value model.name] []]
-          , p [] [button [onClick CreateGame] [text "Create!"]]
-          , ul [] gameItems
-          ]
+      div [] [ div [class "game-controls"]
+                   [ newGameForm model
+                   , gameItems
+                   ]
+             , stateTable
+             ]
+
+newGameForm : Model -> Html Msg
+newGameForm model =
+    div [class "new-game-form"]
+        [ p [] [text "New Game of Life"]
+        , p [] [ input [onInput SetWidth, value (toString model.width)] []
+               , text "x"
+               , input [onInput SetHeight, value (toString model.height)] []
+               ]
+        , p [] [input [onInput SetName, value model.name] []]
+        , p [] [button [onClick CreateGame] [text "Create!"]]
+        ]
+
 
 gameItem : String -> Html msg
 gameItem name =
     li [] [text name]
+
+stateRow : List Bool -> Html msg
+stateRow row =
+    tr [] (List.map stateCell row)
+
+stateCell : Bool -> Html msg
+stateCell cell =
+    td [if cell then class "active-cell" else class "inactive-cell"] []
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
@@ -115,17 +144,24 @@ update msg model =
 
         CreateGame ->
             let
+              (leftSocket, leaveCmd) = leaveCurrentGame model
+              room = "game:" ++ model.name
               payload =
                   Json.Encode.object
                         [ ("width", Json.Encode.int model.width)
                         , ("height", Json.Encode.int model.height)
                         ]
-              channel = Phoenix.Channel.init ("game:" ++ model.name)
+              channel = Phoenix.Channel.init room
                         |> Phoenix.Channel.withPayload payload
-              (phxSocket, phxCmd) = Phoenix.Socket.join channel model.phxSocket
+              (joinedSocket, joinCmd) = Phoenix.Socket.join channel model.phxSocket
+              phxSocket = Phoenix.Socket.on "state_updated" room StateUpdated joinedSocket
             in
-              ( { model | phxSocket = phxSocket }
-              , Cmd.map PhoenixMsg phxCmd
+              ( { model | phxSocket = phxSocket
+                , currentGame = Just room
+                }
+              , Cmd.batch [ Cmd.map PhoenixMsg leaveCmd
+                          , Cmd.map PhoenixMsg joinCmd
+                          ]
               )
 
         PhoenixMsg msg ->
@@ -147,6 +183,25 @@ update msg model =
                     ( model
                     , Cmd.none
                     )
+
+        StateUpdated raw ->
+            case Json.Decode.decodeValue stateUpdatedDecoder raw of
+                Ok stateUpdate ->
+                    ( { model | gameState = stateUpdate.state }
+                    , Cmd.none
+                    )
+
+                Err _ ->
+                    ( model
+                    , Cmd.none
+                    )
+
+leaveCurrentGame model =
+    case model.currentGame of
+        Just currentGame ->
+            Phoenix.Socket.leave currentGame model.phxSocket
+        Nothing ->
+            (model.phxSocket, Cmd.none)
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
