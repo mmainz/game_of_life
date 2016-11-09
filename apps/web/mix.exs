566e5ec72239b25c3306e0f421e64488a06e8299
Mario Mainz <mario.mainz@invision.de>
2016-11-09 23:12:45 +0100

    Add missing umbrella dependency

--- apps/web/mix.exs
+++ apps/web/mix.exs
@@ -42,6 +42,7 @@ defmodule Web.Mixfile do
      {:phoenix_html, "~> 2.7"},
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:gettext, "~> 0.11"},
-     {:cowboy, "~> 1.0"}]
+     {:cowboy, "~> 1.0"},
+     {:game, in_umbrella: true}]
   end
 end
